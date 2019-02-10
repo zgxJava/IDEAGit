78b7793890eae030292cb91d4b6f2e64244b3bfb
zgx.com <zgx@qq.com>
2019-02-10 21:41:35 +0800

    001-Hello-Git

---
+++ src/main/java/com/zgx/git/HelloGit.java
@@ -0,0 +1,19 @@
+package com.zgx.git;
+
+/**
+ * ClassName:HelloGit
+ * Package:com.zgx.git
+ * Description:
+ *
+ * @date:2019/2/10 ${Time}
+ * @author:hhhhh.@qq.com
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello GIT");
+
+    }
+
+}
